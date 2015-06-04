a1a378abb1bc3e78922787200bb050e8f4b9a25d
Burcu Dogan <jbd@google.com>
2015-06-03 17:57:28 -0700

    Delete tests target.

--- drummachineTests/drummachineTests.m
+++ drummachineTests/drummachineTests.m
@@ -1,11 +1,3 @@
-//
-//  drummachineTests.m
-//  drummachineTests
-//
-//  Created by Burcu Dogan on 6/3/15.
-//  Copyright (c) 2015 Developer. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
