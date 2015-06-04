e5a536932268c9023981e265c1fb4e1b71952635
Burcu Dogan <jbd@google.com>
2015-06-03 17:17:20 -0700

    Initial Commit

---
+++ drummachine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  drummachine
+//
+//  Created by Burcu Dogan on 6/3/15.
+//  Copyright (c) 2015 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
